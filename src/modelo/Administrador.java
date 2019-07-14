5829719db8cd80285f4227fdfe80e6456a207bf6
Jessica <Jessica@DESKTOP-9QFGP3H>
2019-07-14 15:17:53 -0500

    Creacion de los atributos del Administrador

--- src/modelo/Administrador.java
+++ src/modelo/Administrador.java
@@ -9,6 +9,46 @@ package modelo;
  *
  * @author Jessica
  */
-public class Administrador {
+
+public class Administrador extends Persona {
     
+    private String admUsername;
+    private String admPasword;
+    
+    
+    
+    /*
+    * Constructor de la Clase Administrador que es subclase de la clase Persona
+    */
+    public Administrador(String admUsername, String admPasword, int perId, String perNombre, 
+                         String perApellido, String perCedula, String perTelCelular, String perTelConvencional, 
+                         String perDireccion, String perCorreo) {
+        
+        super(perId, perNombre, perApellido, perCedula, perTelCelular, perTelConvencional, perDireccion, perCorreo);
+        this.admUsername = admUsername;
+        this.admPasword = admPasword;
+    }
+    
+    
+    /*
+    * Getters y Setters de la clase Administrador
+    */
+
+    public String getAdmUsername() {
+        return admUsername;
+    }
+
+    public void setAdmUsername(String admUsername) {
+        this.admUsername = admUsername;
+    }
+
+    public String getAdmPasword() {
+        return admPasword;
+    }
+
+    public void setAdmPasword(String admPasword) {
+        this.admPasword = admPasword;
+    }
+
 }
+

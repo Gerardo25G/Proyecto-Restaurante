3b79330f865a827cd3712d2517da0ea98e80493d
GerardoG <GerardoG@Gerardo>
2019-07-14 17:17:10 -0500

    Creacion de los atributos de la Clase Producto

--- src/modelo/Producto.java
+++ src/modelo/Producto.java
@@ -11,4 +11,83 @@ package modelo;
  */
 public class Producto {
     
+    private int id;
+    private String nombre;
+    private Double precio;
+    private double peso;
+    private TipoMedida medida;
+    private Categoria categoria;
+
+    /**
+     * Contructor de la clase Producto
+     * @param id
+     * @param nombre
+     * @param precio
+     * @param peso
+     * @param medida
+     * @param categoria 
+     */
+    public Producto(int id, String nombre, Double precio, double peso, TipoMedida medida, Categoria categoria) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.peso = peso;
+        this.medida = medida;
+        this.categoria = categoria;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public TipoMedida getMedida() {
+        return medida;
+    }
+
+    public void setMedida(TipoMedida medida) {
+        this.medida = medida;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+    
+    /**
+     * Setts and Getss
+     */
+    
+    
 }
+

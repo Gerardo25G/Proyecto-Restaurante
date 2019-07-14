53cf736e5bd104d7555ed503ef95f96848f0abdf
Jessica <Jessica@DESKTOP-9QFGP3H>
2019-07-14 17:06:35 -0500

    Creacion de los atributos de la clase Categoria

--- src/modelo/Categoria.java
+++ src/modelo/Categoria.java
@@ -11,4 +11,40 @@ package modelo;
  */
 public class Categoria {
     
+    private int id;
+    private String nombre;
+
+    /**
+     * Constructor de la clase Categoria
+     * @param id
+     * @param nombre 
+     */
+    public Categoria(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+    /**
+     * Gets and Sets
+     */
+    
+    
 }
+

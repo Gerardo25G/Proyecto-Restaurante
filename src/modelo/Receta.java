e8eca5818140a67c2c7a619df69235f44bb657b6
GerardoG <GerardoG@Gerardo>
2019-07-14 17:19:21 -0500

    Creacion de los atributos de la Clase Receta

--- src/modelo/Receta.java
+++ src/modelo/Receta.java
@@ -5,10 +5,65 @@
  */
 package modelo;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author GerardoG
  */
 public class Receta {
+
+    private int id;
+    private String nombre;
+    private Double precio;
+    private ArrayList<RecetaDetalle> recetaDetalle = new ArrayList<>();
+
     
+    /**
+     * 
+     * @param id
+     * @param nombre
+     * @param precio
+     * @param recetaDetalle 
+     */
+    public Receta(int id, String nombre, Double precio, ArrayList<RecetaDetalle> recetaDetalle) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.recetaDetalle = recetaDetalle;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public ArrayList<RecetaDetalle> getRecetaDetalle() {
+        return recetaDetalle;
+    }
+
+    public void setRecetaDetalle(ArrayList<RecetaDetalle> recetaDetalle) {
+        this.recetaDetalle = recetaDetalle;
+    }
+
 }
+

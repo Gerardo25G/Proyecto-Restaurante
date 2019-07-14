9b6e706aa6faa9353e370778fc789f3447f11adc
GerardoG <GerardoG@Gerardo>
2019-07-14 17:23:06 -0500

    Creacion de la los metodos de conexion para la base de datos

--- src/conexion/Conexion.java
+++ src/conexion/Conexion.java
@@ -5,10 +5,49 @@
  */
 package conexion;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author GerardoG
  */
 public class Conexion {
     
+    
+    
+    
+    public static void main(String[] args) {
+        
+        close(getConnection());
+        
+    }
+    
+    public static Connection getConnection() {
+        try {
+        	Class.forName("oracle.jdbc.driver.OracleDriver");  
+        	Connection con = DriverManager.getConnection(  
+                    "jdbc:oracle:thin:@localhost:1521:XE", "RESTAURANTE_ORTIZ","1234");  
+            
+       
+        	System.out.println("Conectado");
+            return con;
+           
+        } catch (Exception ex) {
+            System.out.println("Database.getConnection() Error -->" + ex.getMessage());
+            return null;
+        }
+    }
+ 
+
+    public static void close(Connection con) {
+        try {
+            con.close();
+            System.out.println("Cerrado");
+        } catch (Exception ex) {
+        }
+    }
+    
 }
+

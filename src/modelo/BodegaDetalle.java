3a89f9f5ef9e23f36ec4bd2c389b2a8678a0c673
Jessica <Jessica@DESKTOP-9QFGP3H>
2019-07-14 17:04:28 -0500

    Creacion de los atributos de la clase BodegaDetalle

--- src/modelo/BodegaDetalle.java
+++ src/modelo/BodegaDetalle.java
@@ -10,5 +10,54 @@ package modelo;
  * @author Jessica
  */
 public class BodegaDetalle {
+    private int id;
+    private Double cantidadTotal;
+    private Producto producto;
+
+    
+    /**
+     * Contructor de la clase bodegadetalle
+     * @param id
+     * @param cantidadTotal
+     * @param producto 
+     */
+    public BodegaDetalle(int id, Double cantidadTotal, Producto producto) {
+        this.id = id;
+        this.cantidadTotal = cantidadTotal;
+        this.producto = producto;
+    }
+
+    
+    /**
+     * Setts and Getts
+     * @return 
+     */
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Double getCantidadTotal() {
+        return cantidadTotal;
+    }
+
+    public void setCantidadTotal(Double cantidadTotal) {
+        this.cantidadTotal = cantidadTotal;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+    
+    
     
 }
+

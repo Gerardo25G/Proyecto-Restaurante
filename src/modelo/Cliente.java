5cfa2ebb968a62081e7239805609da922ade307b
GerardoG <GerardoG@Gerardo>
2019-07-25 21:28:55 -0500

    Creacion de la Clase Cliente en el paquete modelo

---
+++ src/modelo/Cliente.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author @author GerardoG
+ */
+public class Cliente extends Persona {
+
+    public Cliente(int perId, String perNombre, String perApellido, String perCedula,
+                  String perTelCelular, String perTelConvencional, String perDireccion, String perCorreo) {
+        super(perId, perNombre, perApellido, perCedula, perTelCelular, perTelConvencional, perDireccion, perCorreo);
+    }
+    
+    
+    
+}
